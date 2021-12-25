832deed840ab63933809fe392fc1ac02cc552c4d
KIMKIHWAN55 <74518262+KIMKIHWAN55@users.noreply.github.com>
2021-12-25 10:31:34 +0900

    Add files via upload

---
+++ JSP_Board_TEST-master/BBS_kihwan/src/board/UserDAO.java
@@ -0,0 +1,63 @@
+package board;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	public UserDAO() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/sys?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			String dbID = "root";
+			String dbPassword = "12341234";
+			Class.forName("com.mysql.cj.jdbc.Driver"); 
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "select userPassword from board where userID= ?";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(userPassword))
+					return 1; 
+				else
+					return 0; 
+			}
+			return -1; 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; 
+	}
+
+
+
+	public int join(Board user) {
+		String SQL = "insert into board values(?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -1; 
+	}
+
+}
